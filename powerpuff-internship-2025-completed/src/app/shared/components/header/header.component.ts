9cd5ff4c89600dd9dd4415657620846669dbb9e0
Sibon Miłosz (YIFO) ext <cjn75@eur.corp.vattenfall.com>
2025-07-30 19:53:07 +0200

    Added a back to home page button

--- powerpuff-internship-2025-completed/src/app/shared/components/header/header.component.ts
+++ powerpuff-internship-2025-completed/src/app/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
     selector: 'app-header',
@@ -8,8 +9,10 @@ import { Component, OnInit } from '@angular/core';
     styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
+  isHome: Observable<boolean> = of(true);
 
   ngOnInit(): void {
-
-  }
-}
+    const currentLocation = window.location.href;
+    if (currentLocation.includes('reactors')) {
+    this.isHome = of(false);
+}}}
